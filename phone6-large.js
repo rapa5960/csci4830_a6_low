a8cf3848673f35ae39f6b1269f1c992b34321d31
rapa5960 <rapa5960@colorado.edu>
2018-03-04 13:23:28 -0700

    Update phone6-large.js

--- phone6-large.js
+++ phone6-large.js
@@ -30,4 +30,46 @@ $("#about_this_project").click(function() { // when "button_id" is clicked
 	$("#content_dialer").hide();	// hide other element
 	$("#content_list").hide();	// hide other element
 });
+
+$( "#1" ).click(function() {
+	$( "#textfield1" ).val($("#textfield1").val() + "1");
+});
+
+$( "#2" ).click(function() {
+	$( "#textfield1" ).val($("#textfield1").val() + "2");
+});
+$( "#3" ).click(function() {
+	$( "#textfield1" ).val($("#textfield1").val() + "3");
+});
+$( "#4" ).click(function() {
+	$( "#textfield1" ).val($("#textfield1").val() + "4");
+});
+$( "#5" ).click(function() {
+	$( "#textfield1" ).val($("#textfield1").val() + "5");
+});
+$( "#6" ).click(function() {
+	$( "#textfield1" ).val($("#textfield1").val() + "6");
+});
+$( "#7" ).click(function() {
+	$( "#textfield1" ).val($("#textfield1").val() + "7");
+});
+$( "#8" ).click(function() {
+	$( "#textfield1" ).val($("#textfield1").val() + "8");
+});
+$( "#9" ).click(function() {
+	$( "#textfield1" ).val($("#textfield1").val() + "9");
+});
+$( "#0" ).click(function() {
+	$( "#textfield1" ).val($("#textfield1").val() + "0");
+});
+$( "#star" ).click(function() {
+	$( "#textfield1" ).val($("#textfield1").val() + "*");
+});
+$( "#pound" ).click(function() {
+	$( "#textfield1" ).val($("#textfield1").val() + "#");
+});
+$( "#clear" ).click(function() {
+	var text = $( this ).text();
+	$( "#textfield1" ).val( "" );
+});
 	
